e2e58e76025f409437daef4798fffe35c1b164f2
Vanessa Ulloa <veedotbebop@gmail.com>
2021-03-06 22:57:33 -0800

    fix class declaration

--- Commands/Entities/Category.py
+++ Commands/Entities/Category.py
@@ -1,23 +1,16 @@
-from dataclasses import dataclass
 from typing import List
-import json
-from json import JSONEncoder
-from collections import namedtuple
 
 
-@dataclass
 class CategoryMember:
     page_id: int
     ns: int
     title: str
 
 
-@dataclass
 class CategoryQuery:
     category_members: List[CategoryMember]
 
 
-@dataclass
 class CategoryRoot:
     batch_complete: str
     category_query: CategoryQuery
