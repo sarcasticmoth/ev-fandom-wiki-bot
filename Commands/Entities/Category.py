d7ab5862a9e5b7f25bb55734365de3d7a2cb2f9b
Vanessa Ulloa <veedotbebop@gmail.com>
2021-03-06 23:11:52 -0800

    fix dataclass reference

--- Commands/Entities/Category.py
+++ Commands/Entities/Category.py
@@ -1,17 +1,20 @@
 from typing import List
+from dataclasses import dataclass
 
 
+@dataclass
 class CategoryMember:
     page_id: int
     ns: int
     title: str
 
 
+@dataclass
 class CategoryQuery:
     category_members: List[CategoryMember]
 
 
+@dataclass
 class CategoryRoot:
     batch_complete: str
     category_query: CategoryQuery
-
